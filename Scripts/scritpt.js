9cc9d0ab0569352b3d282ce127e3e980cd08ff8a
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2021-11-02 20:47:35 +0800

    Update

--- Scripts/scritpt.js
+++ Scripts/scritpt.js
@@ -1,1 +1,16 @@
-console.log("test");
+let weather = {
+
+    "apiKey": "28a63d18f2d7e2f6f49374c2827014de",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apiKey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+
+    
+
+};
