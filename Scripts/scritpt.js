10fe57dd0ecff4e4b83744f2bdd9accee31df4dc
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2021-11-02 22:46:03 +0800

    Alpha version
    date not done

--- Scripts/scritpt.js
+++ Scripts/scritpt.js
@@ -18,7 +18,6 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name,icon,description,temp,humidity,speed,country);
 
         /* DISPLAYS DATA AT THE HTML */
 
@@ -30,9 +29,27 @@ let weather = {
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
-        
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+ name +"')";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
     }
-
-    
-
 };
+
+/* SEARCH FUNCATION */
+
+document
+    .querySelector(".btn")
+    .addEventListener("click", function() {
+        weather.search();
+});
+
+document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchWeather("Manila"); //DEFAULT LOCATION
