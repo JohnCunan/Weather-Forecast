44dbefadb380c9922a58b9ce6a5b8ce5fa9311cd
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2021-11-04 10:51:34 +0800

    Update
    Default background and date and time script added

---
+++ Scripts/date-time.js
@@ -0,0 +1,14 @@
+months = ['January','February','March','April','May','June',
+          'July','August','September','October','November','December']
+
+days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+date_data = new Date();
+
+current_day = date_data.getDay();
+current_month = date_data.getMonth();
+current_date = date_data.getDate();
+current_year = date_data.getFullYear();
+
+document.querySelector(".day").innerText = days[current_day - 1];
+//document.querySelector(".date").innerText = `${months[current_month]} ${current_date}, ${current_year}`
