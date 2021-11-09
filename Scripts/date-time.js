10c0ae254d2ad93c5920e47c9fb8865439f170a0
JohnCunan <76254034+JohnCunan@users.noreply.github.com>
2021-11-09 10:08:31 +0800

    Update date-time.js

--- Scripts/date-time.js
+++ Scripts/date-time.js
@@ -10,7 +10,28 @@ current_month = date_data.getMonth();
 current_date = date_data.getDate();
 current_year = date_data.getFullYear();
 
-document.querySelector(".day").innerText = days[current_day - 1];
-document.querySelector(".date").innerText = `${months[current_month]} ${current_date}, ${current_year}`;
 
-//
+document.querySelector(".day").innerText = 
+    days[current_day - 1] + ",  ";
+document.querySelector(".date").innerText = 
+    `${months[current_month]} ${current_date}, ${current_year}`;
+
+setInterval(time, 500)
+function time() {
+    var d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
+
+    var amPm = ( h < 12 ) ? "AM" : "PM";
+    h = (h > 12) ? h - 12 : h;
+
+    h = ("0" + h).slice(-2);
+    m = ("0" + m).slice(-2);
+    s = ("0" + s).slice(-2);
+
+    document.querySelector(".time").innerHTML = 
+    h + " : " + m +" : " + s + " " + amPm;
+
+    
+}
